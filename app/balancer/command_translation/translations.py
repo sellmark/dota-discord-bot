e161a3ec9883236730752f215e38e16791ed83b1
PogSmok <70848181+PogSmok@users.noreply.github.com>
2024-03-26 17:13:13 +0100

    Remove english translation file entirely

--- app/balancer/command_translation/translations.py
+++ app/balancer/command_translation/translations.py
@@ -7,10 +7,6 @@ TRANSLATIONS = {}
 LANG = "PL"
 
 #import translations
-with open(os.path.join(BASE_DIR, "./app/balancer/command_translation/EN.json"), encoding="utf-8") as english_file:
-    english = json.load(english_file)
-    TRANSLATIONS["EN"] = english
-
 with open(os.path.join(BASE_DIR, "./app/balancer/command_translation/PL.json"), encoding="utf-8") as polish_file:
     polish = json.load(polish_file)
     TRANSLATIONS["PL"] = polish
