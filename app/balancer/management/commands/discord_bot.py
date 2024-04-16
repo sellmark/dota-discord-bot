6f6df740938480b89ebeb28cde201e68469371f7
sellmark <biuro@sellmark.pl>
2024-04-16 12:49:01 +0200

    The same for report tip commands

--- app/balancer/management/commands/discord_bot.py
+++ app/balancer/management/commands/discord_bot.py
@@ -42,6 +42,23 @@ def is_player_registered(msg, dota_id, name):
 def escape_underscore(player_name):
     return player_name.replace("_", "-")
 
+escaping_pattern = (
+            r'^[\w\-\|\\/\s]*'  # Allows alphanumeric, hyphen, underscore, pipe, backslash, forward slash, and spaces
+            r'('
+            r'[\U0001F600-\U0001F64F]'  # Emoticons
+            r'|[\U0001F300-\U0001F5FF]'  # Misc Symbols and Pictographs
+            r'|[\U0001F680-\U0001F6FF]'  # Transport and Map Symbols
+            r'|[\U0001F700-\U0001F77F]'  # Alchemical Symbols
+            r'|[\U0001F780-\U0001F7FF]'  # Geometric Shapes Extended
+            r'|[\U0001F800-\U0001F8FF]'  # Supplemental Arrows-C
+            r'|[\U0001F900-\U0001F9FF]'  # Supplemental Symbols and Pictographs
+            r'|[\U0001FA00-\U0001FA6F]'  # Chess Symbols
+            r'|[\U0001FA70-\U0001FAFF]'  # Symbols and Pictographs Extended-A
+            r'|[\U00002702-\U000027B0]'  # Dingbats
+            r'|[\U000024C2-\U0001F251]'  # Enclosed characters
+            r')*[\w\-\|\\/\s]*$'  # Ending with any amount of alphanumeric and allowed symbols
+        )
+
 class Command(BaseCommand):
     REGISTER_MSG_TEXT = t("register_msg")
 
@@ -1005,6 +1022,7 @@ class Command(BaseCommand):
         await msg.channel.send(t("name_change").format(mention, new_name))
 
     async def rename_myself_command(self, msg, **kwargs):
+        min_length = 3
         max_length = 32
         command = msg.content
         print(f'\n!rename command from {msg.author.name}:\n{command}')
@@ -1020,24 +1038,9 @@ class Command(BaseCommand):
             await msg.channel.send(t("wrong_rename_usage"))
             return
 
-        valid_name_pattern = (
-            r'^[\w\-\|\\/\s]*'  # Allows alphanumeric, hyphen, underscore, pipe, backslash, forward slash, and spaces
-            r'('
-            r'[\U0001F600-\U0001F64F]'  # Emoticons
-            r'|[\U0001F300-\U0001F5FF]'  # Misc Symbols and Pictographs
-            r'|[\U0001F680-\U0001F6FF]'  # Transport and Map Symbols
-            r'|[\U0001F700-\U0001F77F]'  # Alchemical Symbols
-            r'|[\U0001F780-\U0001F7FF]'  # Geometric Shapes Extended
-            r'|[\U0001F800-\U0001F8FF]'  # Supplemental Arrows-C
-            r'|[\U0001F900-\U0001F9FF]'  # Supplemental Symbols and Pictographs
-            r'|[\U0001FA00-\U0001FA6F]'  # Chess Symbols
-            r'|[\U0001FA70-\U0001FAFF]'  # Symbols and Pictographs Extended-A
-            r'|[\U00002702-\U000027B0]'  # Dingbats
-            r'|[\U000024C2-\U0001F251]'  # Enclosed characters
-            r')*[\w\-\|\\/\s]*$'  # Ending with any amount of alphanumeric and allowed symbols
-        )
 
-        if not re.fullmatch(valid_name_pattern, new_name) or len(new_name) > max_length:
+
+        if not re.fullmatch(escaping_pattern, new_name) or len(new_name) > max_length or len(new_name) < min_length:
             await msg.channel.send(t("invalid_nickname").format(self.player_mention(player)))
             return
 
@@ -1640,6 +1643,7 @@ class Command(BaseCommand):
             return t("not_in_this_queue").format(player.name)
 
     async def handle_report_tip_command(self, msg, is_tip: bool, **kwargs):
+        max_length = 128
         # Prevent bot from responding to its own messages
         if msg.author == self.bot.user:
             return
@@ -1673,6 +1677,10 @@ class Command(BaseCommand):
 
             if reporter == reported:
                 await msg.channel.send(t("report_cannot_self"))
+                return
+
+            if not re.fullmatch(escaping_pattern, comment) or len(comment) > max_length:
+                await msg.channel.send(t("invalid_report_comment").format(self.player_mention(reporter)))
                 return
 
             # Call the appropriate method from ReportTipCommands
