aa6f5f1c1bb7b5f155842ac82e2a25f31141bc20
sellmark <biuro@sellmark.pl>
2024-04-17 08:01:08 +0200

    Correct err messages

--- app/balancer/management/commands/discord_bot.py
+++ app/balancer/management/commands/discord_bot.py
@@ -43,21 +43,22 @@ def escape_underscore(player_name):
     return player_name.replace("_", "-")
 
 escaping_pattern = (
-            r'^[\w\-\|\\/\s]*'  # Allows alphanumeric, hyphen, underscore, pipe, backslash, forward slash, and spaces
-            r'('
-            r'[\U0001F600-\U0001F64F]'  # Emoticons
-            r'|[\U0001F300-\U0001F5FF]'  # Misc Symbols and Pictographs
-            r'|[\U0001F680-\U0001F6FF]'  # Transport and Map Symbols
-            r'|[\U0001F700-\U0001F77F]'  # Alchemical Symbols
-            r'|[\U0001F780-\U0001F7FF]'  # Geometric Shapes Extended
-            r'|[\U0001F800-\U0001F8FF]'  # Supplemental Arrows-C
-            r'|[\U0001F900-\U0001F9FF]'  # Supplemental Symbols and Pictographs
-            r'|[\U0001FA00-\U0001FA6F]'  # Chess Symbols
-            r'|[\U0001FA70-\U0001FAFF]'  # Symbols and Pictographs Extended-A
-            r'|[\U00002702-\U000027B0]'  # Dingbats
-            r'|[\U000024C2-\U0001F251]'  # Enclosed characters
-            r')*[\w\-\|\\/\s]*$'  # Ending with any amount of alphanumeric and allowed symbols
-        )
+    r'^[a-zA-Z0-9\-\|\\/\s\!\@\#\$\%\^\&\*\(\)\_\+]*'  # Allows alphanumeric, hyphen, underscore, pipe, backslash, forward slash, spaces, and special characters from SHIFT+1 to SHIFT+0 on a US keyboard, plus pluses
+    r'('
+        r'[\U0001F600-\U0001F64F]'  # Emoticons
+        r'|[\U0001F300-\U0001F5FF]'  # Misc Symbols and Pictographs
+        r'|[\U0001F680-\U0001F6FF]'  # Transport and Map Symbols
+        r'|[\U0001F700-\U0001F77F]'  # Alchemical Symbols
+        r'|[\U0001F780-\U0001F7FF]'  # Geometric Shapes Extended
+        r'|[\U0001F800-\U0001F8FF]'  # Supplemental Arrows-C
+        r'|[\U0001F900-\U0001F9FF]'  # Supplemental Symbols and Pictographs
+        r'|[\U0001FA00-\U0001FA6F]'  # Chess Symbols
+        r'|[\U0001FA70-\U0001FAFF]'  # Symbols and Pictographs Extended-A
+        r'|[\U00002702-\U000027B0]'  # Dingbats
+        r'|[\U000024C2-\U0001F251]'  # Enclosed characters
+    r')*'
+    r'[a-zA-Z0-9\-\|\\/\s\!\@\#\$\%\^\&\*\(\)\_\+]*$'  # Ending with any amount of alphanumeric and allowed symbols
+)
 
 class Command(BaseCommand):
     REGISTER_MSG_TEXT = t("register_msg")
@@ -1643,7 +1644,7 @@ class Command(BaseCommand):
             return t("not_in_this_queue").format(player.name)
 
     async def handle_report_tip_command(self, msg, is_tip: bool, **kwargs):
-        max_length = 128
+        max_length = 160
         # Prevent bot from responding to its own messages
         if msg.author == self.bot.user:
             return
